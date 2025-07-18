33605533a76b2b1162835df572181da035b06e28
Huzaifa Khan <134222820+huzaifa11002@users.noreply.github.com>
2025-07-18 17:27:39 +0500

    Update page.tsx

--- src/app/service/page.tsx
+++ src/app/service/page.tsx
@@ -4,10 +4,11 @@ import About from '../components/about'
 import Clients from '../components/clients'
 import HappyClient from '../components/HappyClient'
 import type { Metadata } from "next";
+import ChooseUs from '../components/ChooseUs'
 
 export const metadata: Metadata = {
-  title: "Influenca - Our Service",
-  description: "We are a Global Influencer Marketing Agency. Our team manages brand collaborations, creates influencer lists, sends emails, and scrapes data. Boost your sales with our help.",
+    title: "Influenca - Our Service",
+    description: "1. Influencer Campaign Management: We plan, execute, and manage end-to-end influencer marketing campaigns tailored to your goals. 2. UGC (User-Generated Content) Creation: High-performing UGC videos for ads, social content, and organic use by experienced creators. 3. Influencer Discovery & Outreach: We source, vet, and negotiate with creators that align with your brand’s values and target audience. 4. Product Seeding & Gifting Campaigns: Organic brand awareness through gifting campaigns to micro and macro influencers. 5. Paid Collab Campaigns: Strategic partnerships with creators for sponsored posts, product reviews, and branded content. 6. Reporting & ROI Analysis: Detailed post-campaign analytics to help you measure performance and optimize future efforts.",
 };
 
 const page = () => {
@@ -20,9 +21,10 @@ const page = () => {
             </div>
             <div className="container mx-auto">
                 <ServiceList />
-                <About />
-                <Clients />
             </div>
+            <About />
+            <Clients />
+            <ChooseUs/>
             <HappyClient />
         </>
     )
